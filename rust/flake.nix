aa29d9b72a67b652e11a640eb99538d2436a109f
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-07-10 17:50:10 +1200

    Rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,3 +1,36 @@
 {
   description = "Liam's Rust template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        # Compile-time dependencies
+        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+
+        # Runtime dependencies
+        buildInputs = with pkgs; [ openssl ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit nativeBuildInputs buildInputs;
+        };
+      }
+    );
 }
